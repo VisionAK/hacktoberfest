02f99313efda2a2f8db5cabe0eec5c6f6e7eed65
ARPIT KHARE <60066282+arpitkhare144@users.noreply.github.com>
2020-10-18 00:42:31 +0530

    hacktoberfest
    hacktoberfest

---
+++ hammingcodeerrorcorection/src/hammingcodeerrorcorection/Sender.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hammingcodeerrorcorection;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Sender {
+    public static void main(String[] args)
+    {
+       
+        int M = str.length(); 
+        int r = 1; 
+  
+        while (Math.pow(2, r) < (M + r + 1)) { 
+            // r is number of redundant bits 
+            r++; 
+        } 
+    }
+}
